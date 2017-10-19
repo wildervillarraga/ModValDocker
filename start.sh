db0c23b22b08cff43d7c38381a5f82921cbda285
Wilder Villarraga <hormigah@gmail.com>
2017-10-18 21:35:09 -0500

    Se actualiza start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 echo "Iniciando..."
 docker build -t img-wilder .
 docker run -d -p 8000:80 --name cont-wilder img-wilder
-echo "Imagen construida"
+echo "Imagen 'img-wilder' construida y contenedor 'cont-wilder' generado"
+echo "La URL es localhost:8000/index.php"
+echo "Para hacer uso del servicio construido agregue los parametros GET 'a' y 'b'. Eje= localhost:8000/index.php?a=2&b=8"
